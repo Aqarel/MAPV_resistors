279ebb61ccd485e2195305846df2533ad4103488
Michal <michal.s@unet.cz>
2013-04-20 15:58:05 +0200

    pridana fce projekt_resistors
    -nacte obrazek
    -vysegmentuje rezistory ( neni 100% při dotyku resistorů + tmave oblasti
    v rohu)
    -vyznačí rezistory v původním obrázku
    - vytvorí matici "oblasti" se souradnicema bodů obdelniku obklopujich
    resistor

---
+++ src/projekt_resistors.m
@@ -0,0 +1,68 @@
+close all;
+clear all;
+
+I  = imread('white_3.png');
+
+
+prah=0.5;
+
+se1 = strel('square',9); % pøi 9 mizej znacky od fixy
+se5 = strel('square',20);
+
+figure;
+
+% vyprahovani a negace výstupu pro dalsi operace (logistejsi mit objekt zajmu bílý)
+I3 = ~im2bw(I,prah); 
+subplot(1,3,1); imshow(I3);
+
+I3_opened = imopen(I3,se1);
+subplot(1,3,2); imshow(I3_opened);
+    
+I3_closed = imclose(I3_opened,se5);
+subplot(1,3,3); imshow(I3_closed);
+
+I_morf = I3_closed; 
+% oznaci cislama jednotlive oblasti
+L = bwlabel(I_morf);
+     
+% [r, c] = find(L==2);
+% rc = [r c];
+
+%*** vymezeni oblasti do obdelniku     
+s  = regionprops(L,'BoundingBox');      
+box = cat(1, s.BoundingBox);
+   
+figure(2);
+imshow(I)
+   hold on
+   
+ % *** vykresleni obdelniku kolem rezistoru do puvodního obrazku ***  
+   for i = 1:length(s)
+ 
+   line ([box(i,1) (box(i,1)+box(i,3))],[box(i,2) (box(i,2))])
+   line ([(box(i,1)+box(i,3)) (box(i,1)+box(i,3))],[box(i,2) (box(i,2)+box(i,4))])
+   line ([(box(i,1)+box(i,3)) box(i,1) ],[(box(i,2)+box(i,4)) (box(i,2)+box(i,4))])
+   line ([box(i,1) box(i,1)],[(box(i,2)+box(i,4)) box(i,2) ])
+   
+   end
+   
+% *** vytvoreni matice bodu obdelnika kolem rezistoru, format Ax Ay Bx By.. 
+%  zacatek levý horní roh a dále po smìru hod. rucicek ***  
+
+for i = 1:length(s)
+    
+    
+    oblasti(i,1) = box(i,1);
+    oblasti(i,2) = box(i,2);
+    oblasti(i,3) = box(i,1)+box(i,3);
+    oblasti(i,4) = box(i,2);
+    oblasti(i,5) = box(i,1)+box(i,3);
+    oblasti(i,6) = box(i,2)+box(i,4);
+    oblasti(i,7) = box(i,2);
+    oblasti(i,8) = box(i,2)+box(i,4);
+   
+      
+end
+  
+   hold off
+
