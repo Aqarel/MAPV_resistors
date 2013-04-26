b7a2dc741eb3d67e50e95ffb8350e7c07094e11f
Milan Kořínek <xp2874@centrum.cz>
2013-04-26 14:52:54 +0200

    Uspesne prohledavani oblasti s rezistorem
    - testovano zatim s jednim Rezistorem.
    - shoda sablony s hledanmy rezistorem je pri korelaci > 0.8
    - nyni bude treba najit nejlepsi vysledek a vratit oblast s rezistorem

--- src/TemplateMatching.m
+++ src/TemplateMatching.m
@@ -3,9 +3,9 @@ close all;
 clear all;
 clc;
 
-
-template = imread('../images/template.png');
-template = template(:,:,1) > 128;
+Icolor  = imread('../images/white_4.png');%imread('../images/black_3.png');
+template = imread('../images/template2.png');
+template = template(:,:,1) < 128;
 % ====== Constant ======
 seOpen = strel('square', 5);
 seClose = strel('square', 8);
@@ -13,8 +13,9 @@ Tsat = 0.35;    % threshold - saturation from HSV
 resL = size(template,2);     % resistor lenght in pixel
 resD = size(template,1);     % resistor diameter in pixel
 
+vArea = 20;                  % virtual area, use for matching beetwen template and test subarea, sometimes template is bigger than found area,
+vAreaD = 6;                  % virtual area diameter, only for finding potential areas with resistor
 
-Icolor  = imread('../images/white_4.png');%imread('../images/black_3.png');
 
 % Detection from RGB  - problem with white_7
 
@@ -43,7 +44,7 @@ box = cat(1, s.BoundingBox);
 areaIndex = 0;
 for i = 1:length(s)
   areaSize = sort(box(i,3:4),'descend');
-  if ((areaSize(1)*areaSize(2) > resL*resD) && (areaSize(2) > resD))   % Area is bigger than area of resistor and smaller size of box is bigger than diameter of resistor
+  if (((areaSize(1)+vArea)*(areaSize(2)+vAreaD) > resL*resD) && ((areaSize(2)+vAreaD) > resD))   % Area is bigger than area of resistor and smaller size of box is bigger than diameter of resistor
     areaIndex(end + 1) = i;                         % index of area
   end
 end
@@ -92,12 +93,46 @@ for j = 1:length(areaIndex)
 end
 hold off
 %% find resistor
-for j = 1:length(areaIndex)
-    i=areaIndex(j);
-    for x = round(box(i,1)):round(box(i,1) + box(i,3))
-        for y = round(box(i,2)):round(box(i,2) + box(i,4))
+activeTempl = ones(size(template,1),size(template,2));  % 
+%for j = 1:length(areaIndex)    % all areas
+    iAr = areaIndex(1);
+    areaX = round(box(iAr,3)); 
+    areaY = round(box(iAr,4)); 
+    area = zeros(areaY+vArea, areaX+vArea);
+    area(1:areaY, 1:areaX) = imBin(round(box(iAr,2)):round(box(iAr,2)+areaY-1), round(box(iAr,1)):round(box(iAr,1)+areaX-1));   % copy found area from image
+    for fi = 0:1:180   % Angle           
+        rotTempl = imrotate(template,fi);
+        rotActiveTempl = imrotate(activeTempl,fi);
+        templX = size(rotTempl,2);
+        templY = size(rotTempl,1);
+        if (templX > (areaX+vArea)) || (templY > (areaY+vArea)) % template is in less one size bigger than area, try other rotation
+           continue; 
+        end
         
+        if (templX >= areaX)
+           stepsX = 1; 
+        else
+           stepsX = areaX - templX;
+        end
+        
+        if (templY >= areaY)
+           stepsY = 1; 
+        else
+           stepsY = areaY - templY;
+        end
+        
+        for y = 1:stepsY % y coord
+            for x = 1:stepsX  % x coord
+                c = corr2_wb(area(y:(y+templY-1),x:(x+templX-1)),rotTempl,rotActiveTempl);
+                figure(1);
+                subplot(2,1,1);
+                imshow(rotTempl);
+                subplot(2,1,2);
+                imshow(area(y:(y+templY-1),x:(x+templX-1)));
+                title(sprintf('X=%d, Y=%d,fi=%d, corr=%f',x,y,fi,c))
+                input('asa');
+            end
         end
     end
-end
+%end
 
--- src/corr2_wb.m
+++ src/corr2_wb.m
@@ -1,4 +1,4 @@
-function R = corr2_wb(A, B)
+function R = corr2_wb(A, B, activeZone)
 %
 tempSize = size(A);
-R = 1 - sum(sum(abs(A - B))) / (tempSize(1) * tempSize(2));
+R = 1 - (sum(sum(abs(A - B).*activeZone))) / (tempSize(1) * tempSize(2));
