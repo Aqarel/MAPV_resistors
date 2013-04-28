46e61ba7ef41db631b83cf995363c99920786223
Milan Kořínek <xp2874@centrum.cz>
2013-04-28 12:21:46 +0200

    Oprava vykreslovani
    - no ukazalo se ze hlavni chyba byla pri vykreslovani ramecku
    - trosku jsem v tom ztracenej, mozna tam je jeste chyba
    - vysledky jsou mnohem lepsi nez pred tim

--- src/TemplateMatching.m
+++ src/TemplateMatching.m
@@ -2,7 +2,7 @@
 close all;
 clear all;
 clc;
-
+tic
 Icolor  = imread('../images/white_1.png');%imread('../images/black_3.png');
 template = imread('../images/template2.png');
 template_b = imread('../images/template2b.png');
@@ -57,10 +57,11 @@ end
 
 areaIndex = areaIndex(2:end);                       % first index is not used
 imshow(imBin)
+toc
 
 %%
 
-   
+tic   
 figure(2);
 imshow(Icolor)
    hold on
@@ -94,6 +95,7 @@ for j = 1:length(areaIndex)
       
 end
 hold off
+toc
 %% find resistor
 figure(3);
 imshow(Icolor);
@@ -106,16 +108,40 @@ for j = 1:length(areaIndex)    % all areas
     area = zeros(areaY+vArea, areaX+vArea);
     area(1:areaY, 1:areaX) = imBin(round(box(iAr,2)):round(box(iAr,2)+areaY-1), round(box(iAr,1)):round(box(iAr,1)+areaX-1));   % copy found area from image
     bestCorr = zeros(areaX,areaY,2); % best correlation on the X, Y across fi; save correlation coeficient and fi
-    
-    for fi = 0:1:180   % Angle           
+    tic
+    step = 2;
+    for fi = 0:step:180   % Angle                  
         rotTempl = imrotate(template,fi);
-        rotTempl_b = imrotate(template_b,fi);
-        rotActiveTempl = imrotate(activeTempl,fi);
+    
+        
+        templX = size(rotTempl,2);
+        templY = size(rotTempl,1);
+        
+        % crop blank space after rotate
+        xSum = sum(rotTempl,1);
+        ySum = sum(rotTempl,2);
+        xOff1 = sum(sum(xSum(1:round(templX/2))==0))+1; % get zeros on start x
+        xOff2 = xOff1;%sum(sum(xSum(round(templX/2):end)==0)); % get zeros on end x
+        yOff1 = sum(sum(ySum(1:round(templY/2))==0))+1; % get zeros on start y
+        yOff2 = yOff1;%sum(sum(ySum(round(templY/2):end)==0)); % get zeros on end y
+        
+        rotTempl = rotTempl(yOff1:end-yOff2,xOff1:end-xOff2);
+        
+        
         templX = size(rotTempl,2);
         templY = size(rotTempl,1);
         if (templX > (areaX+vArea)) || (templY > (areaY+vArea)) % template is in less one size bigger than area, try other rotation
+           step = 2;
            continue; 
         end
+        step = 2;
+        
+        rotTempl_b = imrotate(template_b,fi);
+        rotActiveTempl = imrotate(activeTempl,fi);
+        
+        rotTempl_b = rotTempl_b(yOff1:end-yOff2,xOff1:end-xOff2);
+        rotActiveTempl = rotActiveTempl(yOff1:end-yOff2,xOff1:end-xOff2);
+        
         
         if (templX >= areaX)
            stepsX = 1; 
@@ -128,7 +154,6 @@ for j = 1:length(areaIndex)    % all areas
         else
            stepsY = areaY - templY;
         end
-        
         for y = 1:stepsY % y coord
             for x = 1:stepsX  % x coord
                 temp = rotTempl;
@@ -138,16 +163,22 @@ for j = 1:length(areaIndex)    % all areas
                     bestCorr(x,y,1) = c;
                     bestCorr(x,y,2) = fi;
                 end
-%                 figure(1);
+%                 figure(4);
 %                 subplot(2,1,1);
 %                 imshow(rotTempl);
 %                 subplot(2,1,2);
+%                 hold on
 %                 imshow(area(y:(y+templY-1),x:(x+templX-1)));
+%                 edges = edge(rotActiveTempl,'sobel');
+%                 [yy xx] = find(edges == 1);
+%                 plot(xx, yy, 'b.');
+%                 hold off
 %                 title(sprintf('X=%d, Y=%d,fi=%d, corr=%f',x,y,fi,c))
 %                 input('asa');
             end
         end
     end
+    toc
     % cut blank space
     mask = bestCorr(:,:,1) > 0;
     tmpX = sum(mask,2) + 1;
@@ -161,14 +192,35 @@ for j = 1:length(areaIndex)    % all areas
     
     [y x] = find(bestCorr(:,:,1)==max(max(bestCorr(:,:,1)))); % find max value
     
-    boundaries = bwboundaries(imrotate(activeTempl,bestCorr(y(1),x(1),2)));
+    rotTempl = imrotate(template,bestCorr(y(1),x(1),2));
+    rotActiveTempl = imrotate(activeTempl,bestCorr(y(1),x(1),2));
+    templX = size(rotTempl,2);
+    templY = size(rotTempl,1);
+
+    % crop blank space after rotate
+    xSum = sum(rotTempl,1);
+    ySum = sum(rotTempl,2);
+    xOff1 = sum(sum(xSum(1:round(templX/2))==0))+1; % get zeros on start x
+    xOff2 = sum(sum(xSum(round(templX/2):end)==0)); % get zeros on end x
+    yOff1 = sum(sum(ySum(1:round(templY/2))==0))+1; % get zeros on start y
+    yOff2 = sum(sum(ySum(round(templY/2):end)==0)); % get zeros on end y
+    
+    rotActiveTempl = rotActiveTempl(yOff1:end-yOff2,xOff1:end-xOff2);
+%     
+%     edges = edge(rotActiveTempl,'sobel');
+%     [yy xx] = find(edges == 1);
+%     plot(round(box(iAr,1))+xx-x-(xOff1+xOff2)/4, round(box(iAr,2))+yy-y-(yOff1+yOff2)/4, 'r.');
+    boundaries = bwboundaries(rotActiveTempl);
+
     numberOfBoundaries = size(boundaries);
     
     for k = 1 : numberOfBoundaries
         thisBoundary = boundaries{k};
-        plot(round(box(iAr,1))+y(1)+thisBoundary(:,2), round(box(iAr,2))+x(1)+thisBoundary(:,1), 'r', 'LineWidth', 2);
+        plot(round(box(iAr,1))-x(1)-(xOff1+xOff2)/4+thisBoundary(:,2), round(box(iAr,2))-y(1)-(yOff1+yOff2)/4+thisBoundary(:,1), 'r', 'LineWidth', 2);
     end
 end
 hold off
+
+
 
 
