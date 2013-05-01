6b06304ff35de89ee0743cd45737b91aaffe73e5
Michal <michal.s@unet.cz>
2013-05-02 00:00:10 +0200

    konecne najdu hodnotu odporu rezistoru
    - má to ale svý mouchy
    
    -nedařilo se nastavit zlatou barvu tak, aby to fungovalo i s ní, proto
    jsem ji zanedbal a hledam vsechny ostatní krom zlatý
    
    - to proto, že zlatá se objevuje jen jako 4 pruh
    - a to je problem když bude jina sada...
    
    -na testovací vzorky je to 100%

--- src/najdibarvu.m
+++ src/najdibarvu.m
@@ -1,101 +1,192 @@
 close all;
 clear all;
-vzorekRGB = imread('../images/vzorek7.png');
+vzorekRGB = imread('../images/vzorek14.png');
 hsv = rgb2hsv(vzorekRGB);
-figure;
-imshow(vzorekRGB)
+% figure;
+% imshow(vzorekRGB)
 % 
 % figure;
 % imshow(hsv)
-% 
+
+
+% oblast hledaní barevných pruhù
+seOpen = strel('square', 5);
+seClose = strel('square', 8);
+Tsat = 0.35;
+
+imBin = hsv(:,:,2) > Tsat;     % From saturation
+imBin = imopen(imBin, seOpen);
+imBin = imclose(imBin, seClose);
 % figure;
-% subplot(1,3,1)
-% imshow(hsv(:,:,1))
-% subplot(1,3,2)
-% imshow(hsv(:,:,2))
-% subplot(1,3,3)
-% imshow(hsv(:,:,3))
-
-% h = hsv(:,:,1);
-% s = hsv(:,:,2);
-% v = hsv(:,:,3);
+% imshow(imBin);
 
 
-% vzorek1 pozice prouzku 43 61 83 105 y=15
-% vzorek2 pozice prouzku 40 65 82 107 y=17
-% vzorek3 pozice prouzku 38 65 87 108 y = 15
-% vzorek4 pozice prouzku 40 72 90 110 y = 11
+for i=1:size(imBin,2)
+   
+    sumator_y(1,i) = sum(imBin(:,i)); 
+       
+end
+%zacatek prohledavani
+for i=1:size(sumator_y,2)
+   if sumator_y(1,i) >= size(imBin,1)*3/4
+       pocatek_y = i;
+       break;
+   end
+end
+%konec prohledavani
+for i=size(sumator_y,2):-1:1
+   if sumator_y(1,i) >= size(imBin,1)*3/4
+       konec_y = i;
+       break;
+   end
+end
+
+%vzkresleni ocamcad pocamcad
+figure;
+imshow(vzorekRGB);
+hold on;
+
+
+line([pocatek_y pocatek_y], [1 size(vzorekRGB,1)])
+line([konec_y konec_y], [1 size(vzorekRGB,1)])
+
+
+
+for i=1:size(imBin,1)
+   
+    sumator_x(1,i) = sum(imBin(i,:)); 
+       
+end
+%zacatek prohledavani
+for i=1:size(sumator_x,2)
+   if sumator_x(1,i) >= size(imBin,2)*1/2
+       pocatek_x = i;
+       break;
+   end
+end
+%konec prohledavani
+for i=size(sumator_x,2):-1:1
+   if sumator_x(1,i) >= size(imBin,2)*1/2
+       konec_x = i;
+       break;
+   end
+end
+
+%vzkresleni ocamcad pocamcad
+
+line([1 size(vzorekRGB,2)], [pocatek_x pocatek_x])
+line([1 size(vzorekRGB,2)], [konec_x konec_x])
+hold off;
+
 
 
 x =[10 11 12 13 14 15 16 17 18 19 20 30 31 32 33 34 35 36 37 38 39 40 ];
 j=1;
+k=0;
 
-for n=1:length(x)
+for n= pocatek_x:konec_x
     
-    radek = x(n);
+%     radek = x(n);
     j=1;
-    for i=20:1:size(hsv,2)-20
-    vod1_h= hsv(radek,i,1)*360;
-    vod1_s= hsv(radek,i,2)*100;
-    vod1_v= hsv(radek,i,3)*100;
+    k= k+1;
+    for i=pocatek_y:1:konec_y
+        
+    vod1_h= hsv(n,i,1)*360;
+    vod1_s= hsv(n,i,2)*100;
+    vod1_v= hsv(n,i,3)*100;
 
-        %cerna
-        if ((vod1_h>=0 && vod1_h<=30) ||(vod1_h>=340 && vod1_h<=360) ) && vod1_s>=30 && vod1_s<=100  && vod1_v>=0 && vod1_v<=15
-        barvy(j,n)= 0;
-        j = j+1;  
+%          %pozadi
+%        if vod1_h>=30 && vod1_h<=39 && vod1_s>=55 && vod1_s<=65 && vod1_v>=50 && vod1_v<=65
+%         barvy(k,j)= 88;
+%         j = j+1;  
+%         continue;  
          %hneda
-       elseif vod1_h>=15 && vod1_h<=25 && vod1_s>=80 && vod1_s<=100 && vod1_v>=15 && vod1_v<=30
-        barvy(j,n)= 1;
+      if vod1_h>=10 && vod1_h<=25 && vod1_s>=50 && vod1_s<=100 && vod1_v>=15 && vod1_v<=40
+        barvy(k,j)= 1;
         j = j+1;  
-            
+        continue;    
         % cervena
         elseif ((vod1_h>=0 && vod1_h<=15) ||(vod1_h>=350 && vod1_h<=360) ) && vod1_s>=50 && vod1_s<=100 && vod1_v>=40 && vod1_v<=60
-        barvy(j,n)= 2;
+        barvy(k,j)= 2;
         j = j+1;  
-         
+        continue; 
         % oranzova
         elseif vod1_h>=15 && vod1_h<=25 && vod1_s>=85 && vod1_s<=100 && vod1_v>=55 && vod1_v<=75
-        barvy(j,n)= 3;
+        barvy(k,j)= 3;
         j = j+1;  
-        
+        continue;
         %zluta
         elseif vod1_h>=40 && vod1_h<=50&& vod1_s>=75 && vod1_s<=90 && vod1_v>=50 && vod1_v<=65
-        barvy(j,n)= 4;
+        barvy(k,j)= 4;
         j = j+1;
-      
+      continue;
         % zelena
         elseif vod1_h>=100 && vod1_h<=130 && vod1_s>=50 && vod1_s<=75 && vod1_v>=18 && vod1_v<=30
-        barvy(j,n)= 5;
+        barvy(k,j)= 5;
         j = j+1;
-       
+       continue;
         %modra
-        elseif vod1_h>=190 && vod1_h<=240 && vod1_s>=60 && vod1_s<=85 && vod1_v>=18&& vod1_v<=25
-        barvy(j,n)= 6;
+        elseif vod1_h>=190 && vod1_h<=259 
+        barvy(k,j)= 6;
         j = j+1;
-      
+      continue;
         % fialova
         elseif vod1_h>=260 && vod1_h<=340 && vod1_s>=30 && vod1_s<=60 && vod1_v>=10 && vod1_v<=25
-        barvy(j,n)= 7;
+        barvy(k,j)= 7;
         j = j+1;  
-        
-        %seda
-        elseif vod1_h>=25 && vod1_h<=40 && vod1_s>=30 && vod1_s<=50 && vod1_v>=20&& vod1_v<=40
-        barvy(j,n)= 8;
+        continue;
+       
+ %seda1
+       elseif vod1_h>=25 && vod1_h<=37 && vod1_s>=30 && vod1_s<=50 && vod1_v>=20&& vod1_v<=40 %vod1_v<=40
+        barvy(k,j)= 8;
         j = j+1;
+        continue;
+        
+         %seda2
+       elseif vod1_h>=20 && vod1_h<=35 && vod1_s>=40 && vod1_s<=55 && vod1_v>=25&& vod1_v<=35 %vod1_v<=40
+        barvy(k,j)= 8;
+        j = j+1;
+        continue;
+
+%    zlata     vod1_h>=20 && vod1_h<=35 && vod1_s>=45 && vod1_s<=75 && vod1_v>=20&& vod1_v<=50
+        
+        % zlata 1
+        elseif vod1_h>=30 && vod1_h<=50 && vod1_s>=20 && vod1_s<=60 && vod1_v>=80&& vod1_v<=95
+        barvy(k,j)= 99;
+        j = j+1;
+        continue;
+        
+        % zlata 2
+        elseif vod1_h>=20 && vod1_h<=40 && vod1_s>=40 && vod1_s<=70 && vod1_v>=20&& vod1_v<=50
+        barvy(k,j)= 99;
+        j = j+1;
+        continue;
+        
+        % zlata 3
+        elseif vod1_h>=25 && vod1_h<=40 && vod1_s>=55 && vod1_s<=75 && vod1_v>=50&& vod1_v<=80
+        barvy(k,j)= 99;
+        j = j+1;
+        continue;
+        
+          
+
+        
+        %cerna
+       elseif vod1_v>=0 && vod1_v<=15
+        barvy(k,j)= 0;
+        j = j+1; 
+        continue;
+        
+        
         
         %bila
-        elseif vod1_h>=30 && vod1_h<=50 && vod1_s>=25 && vod1_s<=35 && vod1_v>=70&& vod1_v<=80
-        barvy(j,n)= 9;
+        elseif vod1_s>=10 && vod1_s<=40 && vod1_v>=60&& vod1_v<=100
+        barvy(k,j)= 9;
         j = j+1;
-        
-        % zlata
-        elseif vod1_h>=20 && vod1_h<=35 && vod1_s>=45 && vod1_s<=75 && vod1_v>=25&& vod1_v<=45
-        barvy(j,n)= 10;
-        j = j+1;
-        
+        continue;
         % ani jedna 
         else
-            barvy(j,n)= 99;
+            barvy(k,j)= 99;
             j = j+1;
      end
     end
@@ -103,85 +194,83 @@ end
    
     barvy;
 
-
-%odstraneni samostatných
-m=1;
-o=1;
-
-for o=1:size(barvy,2)
-    m=1;
-for k=1:(length(barvy())-1)
+    povolene_pasmo_horni = barvy(1:(size(barvy,1)/2)-5,:); 
+    povolene_pasmo_dolni = barvy((size(barvy,1)/2)+5:end,:); 
     
-   if barvy(k+1,o)==barvy(k,o)
-      retezec1(m,o) = barvy(k,o);
-      m=m+1;
+    % modusy jednotlivých pasem
+  
+    for i=1:size( povolene_pasmo_horni,2)
+    modus_povolene_pasmo_horni(1,i)= mode(povolene_pasmo_horni(:,i));
+    end
+    
+    % modusy jednotlivých pasem
+  
+    for i=1:size( povolene_pasmo_dolni,2)
+    modus_povolene_pasmo_dolni(1,i)= mode(povolene_pasmo_dolni(:,i));
+    end
+    
+    % modus z jednotlivých sloupecku
+    
+    modus_celk(1,:)=modus_povolene_pasmo_horni;
+    modus_celk(2,:)=modus_povolene_pasmo_dolni;
    
-   end
-end
-end
-
-retezec1;
-
-
-%odstraneni nepotrebných
-
-m=1;
-hodnota=999;
-
-
-for p=1:size(retezec1,2)
-    hodnota=999;
-    m=1;
-    for k=1:size(retezec1,1)
     
-   if retezec1(k,p)~=hodnota
-      retezec2(m,p) = retezec1(k,p);
+    j=1;
+    for i=1:size(modus_celk,2)
+        if modus_celk(1,i)== modus_celk(2,i)
+            modus(1,j)=modus_celk(1,i);
+            j=j+1;
+        end
+    end
+    modus;
+    
+    % zjisteni jak je natocenej
+    for i=1:size(modus,2)
+        if modus(1,i)~=99;
+            vzdalenost_pocatek = i;
+            break;
+        end
+    end
+    
+    for i=size(modus,2):-1:1
+        if modus(1,i)~=99;
+            vzdalenost_konec =size(modus,2)- i;
+            break;
+        end
+    end
+    
+    % je li treba, prohodim pole
+ rotace=0;
+ 
+    if vzdalenost_pocatek > vzdalenost_konec
+        
+        pomocna = modus;
+        rotace =1;
+       
+        for i=1: size(modus,2)
+        modus(1,i)= pomocna(1,size(modus,2)-i+1);
+       end
+    end
+ modus; 
+% vysosnuti od kazdyho jedno 
+    m=1;
+    aktualni=789;
+    predchozi=456;
+    
+    for k=1:size(modus,2)
+      aktualni=modus(1,k);
+      
+     if aktualni~=predchozi
+      retezec_hodnot(1,m) = aktualni;
       m=m+1;
-      hodnota=retezec1(k,p);
+      predchozi=aktualni;
    
      end
     end
-end
-
- retezec2;
-
-%výber relativních
-s=1;
-if size(retezec2,1)>=10
-for r=1:size(retezec2,2)
-   
-    if retezec2(9,r)==99 && retezec2(10,r)==0 
-      retezec3(1:8,s) =  retezec2(1:8,r); 
-        s= s+1;
-    
-    end
-    
-end
-else retezec3(1:8,s)=0;
-end
-
-
-% hodnota odporu
-i=1;
-for u=2:2:8
-kod = retezec3(u,:);
-vysledek(i)= mode(kod);
-i=i+1;
-end
- 
-% je-li natocen obracene, prohodi hodnoty
-
-if vysledek(1)==10
-   pomocna(1)=vysledek(4);
-   pomocna(2)=vysledek(3);
-   pomocna(3)=vysledek(2);
-   pomocna(4)=vysledek(1);
-   vysledek = pomocna;
-end
-
+retezec_hodnot; 
 
 % urèení násobku
-switch(vysledek(4))
+switch(retezec_hodnot(1,6))
    case 10
       nasobek= 0.1;
    case 11
@@ -204,4 +293,7 @@ switch(vysledek(4))
       nasobek= 10000000;
 end
 
-hodnota_odporu = (vysledek(1)*100 + vysledek(2)*10+vysledek(3)*1)*nasobek
+hodnota_odporu = (retezec_hodnot(1,2)*10 + retezec_hodnot(1,4))*nasobek
+hodnota_odporu_str = num2str(hodnota_odporu)
+text(10,10,hodnota_odporu_str)
+
